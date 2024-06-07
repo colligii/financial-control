f44148878bf3dfa28477b788ff98adcce9722027
Giovanne Colli <cgiovanne45@gmail.com>
2024-06-07 19:49:30 -0300

    first commit

---
+++ src/components/SimulatorFrame.vue
@@ -0,0 +1,13 @@
+<script>
+    export default {
+        name: 'SimulatorFrame'
+    }
+</script>
+
+<template>
+    <div>
+        <h1>
+            Simulador de Juros Compostos
+        </h1>
+    </div>
+</template>
---
+++ src/router/index.ts
@@ -0,0 +1,15 @@
+import Main from '@/views/Main.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Main,
+    }
+  ]
+})
+
+export default router
